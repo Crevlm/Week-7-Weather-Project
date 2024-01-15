3baa515b837b6b0146428f9e01858b5389bb6d5e
Crev <crevlm@gmail.com>
2024-01-15 16:02:47 -0500

    Added API for time, weather description

--- src/script.js
+++ src/script.js
@@ -2,10 +2,40 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+
+  descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
+  timeElement.innerHTML = formatDate(date);
+
+  function formatDate (date) {
+   
+  
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+  }
 
   temperatureElement.innerHTML = Math.round(temperature);
+ 
 }
+
+
+
 
 function searchCity(city) {
   let apiKey = "23a97cd4bfcbff879400f60t20ao904e";
