b4ac49e8509c8586831b4dcb31a73c412beca6cf
Crev <crevlm@gmail.com>
2024-01-15 01:52:59 -0500

    Updated JS page with  functions for Search form to include API

--- src/script.js
+++ src/script.js
@@ -1,28 +1,25 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.city;
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
 
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
-
 function searchCity(city) {
-let apiKey = "23a97cd4bfcbff879400f60t20ao904e";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
-axios.get(apiUrl).then(refreshWeather);
+  let apiKey = "23a97cd4bfcbff879400f60t20ao904e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
 }
-
 
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    console.log(searchInput.value);
-    
- searchCity(searchInput.value);
-}
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.value);
 
+  searchCity(searchInput.value);
+}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
