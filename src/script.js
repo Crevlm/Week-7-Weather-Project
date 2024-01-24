e0451cd933d5c9c16d5ec8ddf518ae26314a87ad
Crev <crevlm@gmail.com>
2024-01-24 16:50:31 -0500

    Fixed Format issues with Forecast JS

--- src/script.js
+++ src/script.js
@@ -7,10 +7,9 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-let iconElement = document.querySelector("#icon");
+  let iconElement = document.querySelector("#icon");
 
-
-icon.innerHTML = `<img src = "${response.data.condition.icon_url}" class="icon" />`;
+  icon.innerHTML = `<img src = "${response.data.condition.icon_url}" class="icon" />`;
 
   descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
@@ -18,29 +17,31 @@ icon.innerHTML = `<img src = "${response.data.condition.icon_url}" class="icon" 
   windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
   timeElement.innerHTML = formatDate(date);
 
-  function formatDate (date) {
-   
-  
+  function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  let day = days[date.getDay()];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
 
-  if (minutes < 10) {
-minutes = `0${minutes}`;
-  }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
 
-  return `${day} ${hours}:${minutes}`;
+    return `${day} ${hours}:${minutes}`;
   }
 
   temperatureElement.innerHTML = Math.round(temperature);
- 
 
   getForecast(response.data.city);
 }
-
-
-
 
 function searchCity(city) {
   let apiKey = "23a97cd4bfcbff879400f60t20ao904e";
@@ -57,43 +58,26 @@ function handleSearchSubmit(event) {
 
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-   
-
-  
 }
 searchCity("Jacksonville");
 
-
-
-
-
-
-
-
 function getForecast(city) {
   let apiKey = "23a97cd4bfcbff879400f60t20ao904e";
-  let apiUrl =
-    `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
 
-
-    axios.get(apiUrl).then(displayForecast);
-  
+  axios.get(apiUrl).then(displayForecast);
 }
 
-
-  function displayForecast(response) {
-console.log(response.data);
-
-
+function displayForecast(response) {
+  console.log(response.data);
 
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-let forecastHtml = "";
+  let forecastHtml = "";
 
-
-days.forEach(function(day) {
-   forecastHtml += `
+  days.forEach(function (day) {
+    forecastHtml += `
    <div class="weather-forecast-date">
+   <div>
             ${day}
             </div>
             <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" width="40px" alt="">
@@ -104,13 +88,9 @@ days.forEach(function(day) {
           </div>  
         </div>
 `;
-  
-
-});
+  });
   let forecastElement = document.querySelector("#forecast");
-forecastElement.innerHTML = forecastHtml;
+  forecastElement.innerHTML = forecastHtml;
+}
 
-  }
-
-
-  displayForecast();
+displayForecast();
